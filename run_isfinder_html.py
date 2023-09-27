9556cc08850d56f55037c44a47544a7a69f67f42
YAYA <61085371+HZAU-YAYA@users.noreply.github.com>
2023-09-28 00:13:41 +0800

    Update run_isfinder_html.py

--- run_isfinder_html.py
+++ run_isfinder_html.py
@@ -48,7 +48,7 @@ def run_IS605_txt(html, prefix, path=os.getcwd()):
             row_data.extend([link1, link2, link3])
             table_data.append(row_data)
     print(table_data)
-    with open(path+prefix+'_out.tsv', 'w', encoding='utf-8') as tsvfile:
+    with open(path+'/'+prefix+'_out.tsv', 'w', encoding='utf-8') as tsvfile:
         # 写入表头
         tsvfile.write('\t'.join(header_info) + '\n')
         # 写入表格数据
@@ -58,7 +58,7 @@ def run_IS605_txt(html, prefix, path=os.getcwd()):
 def run_link(prefix, path=os.getcwd()):
     requests.packages.urllib3.disable_warnings()
     data = {}
-    with open(path+prefix+'_out.tsv', 'r', encoding='utf-8') as file:
+    with open(path+'/'+prefix+'_out.tsv', 'r', encoding='utf-8') as file:
         next(file)
         lines = file.readlines()
     extracted_data = []
@@ -84,8 +84,17 @@ def run_link(prefix, path=os.getcwd()):
         LE = article.find_all('span', class_='seq')[0].text.strip() if len(article) >0 else ''
         RE = article.find_all('span', class_='seq')[1].text.strip() if len(article) >0 else ''
         DNA = article.find_all('div', class_='seq')[0].text.strip() if len(article) >0 else ''
-        orf = article.find_all('section')[5]
-        orf_n = orf.find_all('span', class_='entete_propriete', string='ORF number : ')[0].find_next_sibling(string=True).strip()
+        for section in soup.find_all('section'):
+            # 查找包含 ORF 编号的 <span> 元素
+            orf = section.find('span', class_='entete_propriete', string='ORF number : ')
+            if orf:
+                # 获取 ORF 编号的值
+                orf_n = orf.find_next_sibling(string=True).strip()
+                if orf_n:
+                    orf = section
+                    break
+        # orf = article.find_all('section')[5]
+        # orf_n = orf.find_all('span', class_='entete_propriete', string='ORF number : ')[0].find_next_sibling(string=True).strip()
         data[IS_name] = [host, LE, RE, DNA, orf_n]
         if int(orf_n) > 0:
             for i in range(1, int(orf_n) + 1):
@@ -93,14 +102,44 @@ def run_link(prefix, path=os.getcwd()):
                 num = oo.find_next_sibling(string=True).strip()
                 ORF_function = oo.find_next('span', class_='entete_propriete',string='ORF function : ').find_next_sibling(string=True).strip()
                 na = oo.find_next('span', class_='entete_propriete', string='ORF function : ').find_next('span', class_='entete_propriete').find_next_sibling(string=True).strip()
+                table = oo.find_next('table')
+                rows = table.find_all('tr')
+                th = rows[0].find_all('th')
+                if len(th) != 5:
+                    print('Error: The table format is non-standard')
+                if th[0].text.strip() == 'Length':
+                     length = rows[1].find_all('td')[0].text.strip() + '|' + rows[1].find_all('td')[1].text.strip()
+                else:
+                    length = ''
+                    print('Error: length is not found')
+                if th[1].text.strip() == 'Begin':
+                     begin = rows[1].find_all('td')[2].text.strip()
+                else:
+                    begin = ''
+                    print('Error: begin is not found')
+                if th[2].text.strip() == 'End':
+                     end = rows[1].find_all('td')[3].text.strip()
+                else:
+                    end = ''
+                    print('Error: end is not found')
+                if th[3].text.strip() == 'Strand':
+                     strand = rows[1].find_all('td')[4].text.strip()
+                else:
+                    strand = ''
+                    print('Error: strand is not found')
+                if th[4].text.strip() == 'Fusion ORF':
+                     fusion_orf = rows[1].find_all('td')[5].text.strip()
+                else:
+                    fusion_orf = ''
+                    print('Error: fusion_orf is not found')
                 if oo.find_next('p', class_='entete_propriete',string='ORF sequence : '):
                     ORF_protein = oo.find_next('div', class_='seq').text.strip()
-                data[IS_name].append([num, ORF_function, na, ORF_protein])
+                data[IS_name].append([num, ORF_function, na, ORF_protein, length, begin, end, strand, fusion_orf])
         elif int(orf_n) == 0:
             print(IS_name+' has no ORF')
         #pwd = os.getcwd()
         for II in data:
-            folder_path = str(path+ '/' + prefix + '_db/' + II + 'ORF')
+            folder_path = str(path+ '/' + prefix + '_db/' + II)
             os.makedirs(folder_path, exist_ok=True)
             if os.path.exists(folder_path):
                 if int(data[II][4]) == 0:
@@ -109,18 +148,24 @@ def run_link(prefix, path=os.getcwd()):
                         fasta.write(data[II][3])
                     with open(folder_path + '/' + II + '_LE_RE.tsv', 'w') as tsv:
                         tsv.write(II + '\n')
-                        tsv.write('\t'.join(data[II][0:2]))
+                        tsv.write('\n'.join(data[II][0:3]))
                 elif int(data[II][4]) > 0:
                     with open(folder_path + '/' + II + '_seq.fasta', 'w') as fasta:
                         fasta.write('>' + II + '\n')
                         fasta.write(data[II][3])
                     with open(folder_path + '/' + II + '_LE_RE.tsv', 'w') as tsv:
                         tsv.write(II + '\n')
-                        tsv.write('\t'.join(data[II][0:2]))
+                        tsv.write('\n'.join(data[II][0:3]))
+                    with open(folder_path + '/' + II + '_ORF' + '.tsv', 'w') as tsv:
+                        tsv.write('IS\tGene\tORF_function\tlength\tStart\tEnd\tLength\tCoverage\n')
                     for iii in range(int(data[II][4])):
                         with open(folder_path + '/' + II + '_' + data[II][iii+5][2] + '_ORF' + str(iii + 1) + '.fasta', 'w') as fa:
-                            fa.write('>' + II + '_' + data[II][iii+5][2] + '_ORF' + '\t' + data[II][iii+5][1] + '\n')
+                            fa.write('>' + II + '\t' + data[II][iii+5][2] + '\t' + data[II][iii+5][1] + '\n')
                             fa.write(data[II][iii+5][3])
+                        with open(folder_path + '/' + II + '_ORF' +'.tsv', 'a') as tsv:
+                            tsv.write(II + '\t' + data[II][iii+5][2] + '\t' + data[II][iii+5][1] + '\t' +
+                                      data[II][iii+5][4] + '\t' + data[II][iii+5][5] + '\t' +
+                                      data[II][iii+5][6] + '\t' + data[II][iii+5][7] + '\t' + data[II][iii+5][8]+'\n')
             else:
                 print('ERROR:无法创建目录')
                 break
@@ -141,3 +186,4 @@ contact: %s <%s>\
 
 if __name__ == "__main__":           #固定格式，使 import 到其他的 python 脚本中被调用（模块重用）执行
     main()
+
